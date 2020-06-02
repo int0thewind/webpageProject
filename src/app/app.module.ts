38e02647a659213eb25beeb824070b2504ed2731
Hanzhi Yin <yinhanzhi@gmail.com>
2020-06-02 17:35:23 -0500

    reorder app module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-
 // Angular Material Module Import
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -19,7 +11,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { LayoutModule } from '@angular/cdk/layout';
-import { VersionDialogComponent } from './version-dialog/version-dialog.component';
 
 const angularMaterialModules = [
   MatButtonModule,
@@ -36,6 +27,15 @@ const angularMaterialModules = [
   MatDialogModule,
   LayoutModule,
 ];
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { VersionDialogComponent } from './version-dialog/version-dialog.component';
 
 @NgModule({
   declarations: [
