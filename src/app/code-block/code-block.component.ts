b5fde12442cad7917cfbc0feead17b7a1332d6ed
Hanzhi Yin <yinhanzhi@gmail.com>
2020-06-02 21:35:26 -0500

    fix an error in code block css

--- src/app/code-block/code-block.component.ts
+++ src/app/code-block/code-block.component.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 <code [class]="language"><ng-content></ng-content></code>
 </pre>
   `,
+  styles: [`pre > code { width: min(80vw, 1000px); margin: auto; }`]
 })
 export class CodeBlockComponent {
   @Input() language: string;
